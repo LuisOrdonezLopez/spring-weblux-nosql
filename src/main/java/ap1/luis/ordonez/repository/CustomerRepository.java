08297e212ae6141db00f7873cb8c06366608c8c8
Luis Ordoñez Lopez <luis.ordonez.lopez@vallegrande.edu.pe>
2026-03-19 14:47:50 +0000

    first AP1 class v1

--- src/main/java/ap1/luis/ordonez/repository/CustomerRepository.java
+++ src/main/java/ap1/luis/ordonez/repository/CustomerRepository.java
@@ -1,6 +1,6 @@
 package ap1.luis.ordonez.repository;
 
-import pe.edu.vallegrande.app.model.Customer;
+import ap1.luis.ordonez.model.Customer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 public interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
